bb110f746524105fe510592db13115335ef08b3e
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-09 14:02:21 -0400

    pluh

--- src/cli/stb_impl.c
+++ src/cli/stb_impl.c
@@ -1,3 +1,8 @@
+#if defined(__GNUC__) || defined(__clang__)
+#pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wextra"
+#endif
+
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_NO_STDIO
 #include "stb_image.h"
